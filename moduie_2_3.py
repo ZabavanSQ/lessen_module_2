2d6c8787456428def73a450836ed1ca7b3e78e0d
ZabavanSQ <zabavnsq@mail.ru>
2024-08-09 02:22:09 +0300

    Исправленное задание

--- moduie_2_3.py
+++ moduie_2_3.py
@@ -1,12 +1,14 @@
-my_list = [42, 69, 322, 13, 0, 99, 5, -9, 8, -7, 6, 5]
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 n = my_list.__len__()
 x = 0
 while my_list[x] >= 0:
     valy = my_list[x]
-    print(valy)
-    x = x + 1
+    if my_list[x] > 0:
+        print(valy)
+        x = x + 1
+    else:
+        x = x + 1
+        continue        
     if x >= n:
         break
 print('Конец')
-
-
