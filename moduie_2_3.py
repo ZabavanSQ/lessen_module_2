bca785548c1e014d1ad0bd6546b370e0cbf7b186
ZabavanSQ <zabavnsq@mail.ru>
2024-08-09 00:48:25 +0300

    Update moduie_2_3.py
    mod

--- moduie_2_3.py
+++ moduie_2_3.py
@@ -1,7 +1,7 @@
-my_list = [42, 69, 322, 13, 0, 99, 5, -9, 8, 7, 6, 5]
+my_list = [42, 69, 322, 13, 0, 99, 5, -9, 8, -7, 6, 5]
 n = my_list.__len__()
 x = 0
-while my_list[x] >= 0 and x <= n:
+while my_list[x] >= 0:
     valy = my_list[x]
     print(valy)
     x = x + 1
