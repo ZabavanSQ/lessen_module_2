6edfff271e8c80cf8930d252db248fcb50ef5b3b
ZabavanSQ <zabavnsq@mail.ru>
2024-08-09 00:26:12 +0300

    Стиль кода. Цикл While

---
+++ moduie_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, 5, -9, 8, 7, 6, 5]
+n = my_list.__len__()
+x = 0
+while my_list[x] >= 0 and x <= n:
+    valy = my_list[x]
+    print(valy)
+    x = x + 1
+    if x >= n:
+        break
+print('Конец')
+
+
