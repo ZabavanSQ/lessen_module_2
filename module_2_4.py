44d1564f48e24dc0a81274366be0ef72b02b46be
ZabavanSQ <zabavnsq@mail.ru>
2024-08-11 22:03:12 +0300

    Цикл For

---
+++ module_2_4.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in range(len(numbers)):
+    is_primes = True
+    for j in 2, 3, 5, 7:
+        if numbers[i] % j == 0 and numbers[i] == j:
+            is_primes = True
+            break
+        if numbers[i] % j == 0:
+                    is_primes = False
+    if is_primes == True:
+        primes.append(numbers[i])
+    else:
+        not_primes.append(numbers[i])
+print(f'Простые числа = {primes}')
+print(f'Не простые числа = {not_primes}')
